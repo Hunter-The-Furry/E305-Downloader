4df320a963de2011ca137f51a9ceb87e9805b6bd
Hunter The Protogen <39228724+Hunter-The-Furry@users.noreply.github.com>
2020-12-14 20:32:47 -0500

    Revert "Revert "idk what i'm doing send help""
    This reverts commit 918aa9361d22e01bbbcaa24ed588df2bcd8c4e7f.

---
+++ py621/run.py
@@ -0,0 +1,27 @@
+import sys
+import py621
+import yaml
+from requests_oauthlib import OAuth1
+
+def runPy621():
+    with open('settings.yml', 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    if cfg['username'] == 'Your username here' or cfg["api_key"] == 'your api key here' or cfg["tags"] == ["your", "tags", "here"]:
+        print("\nPlease edit the settings.yml file before you continue\n")
+        sys.exit(0)
+    username = cfg['username']
+    api_key = cfg['api_key']
+    tags = cfg['tags']
+    limit = cfg['files_to_download']
+    download_to = "downloads/"+cfg['download_location']
+    if cfg['mode'] == 'E621' or cfg['mode'] == 'e621':
+        isSafe = False
+    else:
+        isSafe = True
+    download_pools = cfg['download_pools']
+    downloadActive = cfg["download_active"]
+    poolName = cfg["pool_name"]
+    check = cfg['check_tags']
+    download_path = cfg['downloads_path']
+    auth = (username, api_key)
+    py621.download.downloadPosts(isSafe, tags, limit, check, auth, download_to, download_path, download_pools, downloadActive, poolName)
